496d81c31b57aebc747afcce740784131e2b971a
Jeffrey Chen <jeffreychen@Jeffreys-MacBook-Pro.local>
2017-07-14 14:36:17 -0700

    add screen color change

--- frontend/expo/App.js
+++ frontend/expo/App.js
@@ -24,9 +24,10 @@ function Button(props) {
 export default class App extends React.Component {
     state = {
         waiting: false,
+        validated: false
     };
+    componentDidMount() {
 
-    render() {
         let authFunction;
 
         if (Platform.OS === 'android') {
@@ -49,95 +50,105 @@ export default class App extends React.Component {
                     'Use Touch ID to log in!'
                 );
                 if (result.success) {
-                    alert('Success!');
+                    this.setState({
+                        validated: true
+                    })
+
                 } else {
-                    alert('Cancel!');
                 }
             };
         }
 
-        return (
-            <View style={[{flex: 1}, styles.center]}>
-            <Button onPress={authFunction} style={[styles.button, styles.buttonBlue, styles.center]}>
-            {this.state.waiting
-                ? 'Waiting for fingerprint... '
-                : 'Authenticate with fingerprint'}
-                </Button>
-                </View>
-            );
-        }
+        authFunction();
     }
+    render() {
 
+        return (
+            <View style={[
+                {flex: 1},
+                styles.center,
+                {backgroundColor: this.state.validated ? '#66ff66' : '#ff4d4d'}
+            ]}>
+            </View>
+        )
+    }
+}
 
-    const styles = StyleSheet.create({
-        center: {
-            justifyContent: 'center',
-            alignItems: 'center'
-        },
-        list: {
-            width: Dimensions.get('window').width*0.9,
-            borderRadius: 4,
-            borderWidth: 0.5,
-            borderColor: '#d6d7da',
-            padding: 4
-        },
-        userList: {
-            width: Dimensions.get('window').width*0.9,
-            borderRadius: 4,
-            borderWidth: 0.5,
-            borderColor: '#d6d7da',
-            padding: 8,
-            paddingLeft: 16
-        },
-        container: {
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-            backgroundColor: '#F5FCFF',
-            borderColor: 'black'
-        },
-        containerFull: {
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'stretch',
-            backgroundColor: '#F5FCFF'
-        },
-        welcome: {
-            fontSize: 20,
-            textAlign: 'center',
-            margin: 10,
-        },
-        instructions: {
-            textAlign: 'center',
-            color: '#333333',
-            marginBottom: 5,
-        },
-        textBig: {
-            fontSize: 36,
-            textAlign: 'center',
-            margin: 10,
-        },
-        button: {
-            alignSelf: 'stretch',
-            paddingTop: 10,
-            paddingBottom: 10,
-            marginTop: 10,
-            marginLeft: 5,
-            marginRight: 5,
-            borderRadius: 5
-        },
-        buttonRed: {
-            backgroundColor: '#FF585B',
-        },
-        buttonBlue: {
-            backgroundColor: '#0074D9',
-        },
-        buttonGreen: {
-            backgroundColor: '#2ECC40'
-        },
-        buttonLabel: {
-            textAlign: 'center',
-            fontSize: 16,
-            color: 'white'
-        }
-    });
+// <Button /*onPress={authFunction} style={[styles.button, styles.buttonBlue, styles.center]}*/>
+// {this.state.waiting
+//     ? 'Waiting for fingerprint... '
+//     : 'Authenticate with fingerprint'}
+//     </Button>
+
+const styles = StyleSheet.create({
+    center: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    list: {
+        width: Dimensions.get('window').width*0.9,
+        borderRadius: 4,
+        borderWidth: 0.5,
+        borderColor: '#d6d7da',
+        padding: 4
+    },
+    userList: {
+        width: Dimensions.get('window').width*0.9,
+        borderRadius: 4,
+        borderWidth: 0.5,
+        borderColor: '#d6d7da',
+        padding: 8,
+        paddingLeft: 16
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+        borderColor: 'black'
+    },
+    containerFull: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'stretch',
+        backgroundColor: '#F5FCFF'
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+    textBig: {
+        fontSize: 36,
+        textAlign: 'center',
+        margin: 10,
+    },
+    button: {
+        alignSelf: 'stretch',
+        paddingTop: 10,
+        paddingBottom: 10,
+        marginTop: 10,
+        marginLeft: 5,
+        marginRight: 5,
+        borderRadius: 5
+    },
+    buttonRed: {
+        backgroundColor: '#FF585B',
+    },
+    buttonBlue: {
+        backgroundColor: '#0074D9',
+    },
+    buttonGreen: {
+        backgroundColor: '#2ECC40'
+    },
+    buttonLabel: {
+        textAlign: 'center',
+        fontSize: 16,
+        color: 'white'
+    }
+});
