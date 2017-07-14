b04e0e932c166ebb14615239ce0adc5ab785e56d
Jeffrey Chen <jeffreychen@Jeffreys-MacBook-Pro.local>
2017-07-14 12:39:31 -0700

    add expo

---
+++ app/components/Initializer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+
+} from 'react-native';
+import TouchID from 'react-native-touch-id';
+import { StackNavigator } from 'react-navigation';
+
+class Initializer extends React.Component {
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return(
+      <div>Your Init Here</div>
+    )
+  }
+}
+
+export default Initializer;
