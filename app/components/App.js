247ea910e9929e6e0e6dafa1a27f52daa25e8d75
Jeffrey Chen <jeffreychen@Jeffreys-MacBook-Pro.local>
2017-07-14 12:33:28 -0700

    Add native app components

--- app/components/App.js
+++ app/components/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import {
+
+} from 'react-native';
+import Initializer from './Initializer';
+import { StackNavigator } from 'react-navigation';
 
 class App extends React.Component {
   constructor(props){
     super(props)
   }
-
   render(){
     return(
       <div>Your App Here</div>
+      <Initializer/>
     )
   }
 }
 
-export default App;
+export default StackNavigator ({
+    App: {
+        screen: App
+    }
+}, { initialRouteName: 'App' });
