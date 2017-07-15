6e74557ba178d1e2dfc400cec7d6d771be6c0865
Prateek-Agarwal-20 <prateeka@sas.upenn.edu>
2017-07-14 22:23:14 -0700

    minro changes

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,26 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var mongoose = require('mongoose')
+var mongoose = require('mongoose');
 
-// var socketMap: Object for storing partenr sockets   token: {t1: t2, t2: t1, authorizing: false, timeoutId}
+var socketMap = {}; /* token: {t1: t2, t2: t1, authorizing: false, timeoutId} */
 
 mongoose.connect("mongodb://onetouch:wedeservetowin@ds135519.mlab.com:35519/one-touch", function() {console.log('connected to mongo');});
 
-
 io.on('connection', function(socket){
+  console.log("New socket connected!", socket.id);
   // Catch identity emission and store in var socketMap
+  socket.on('register_t1', function(emailObj){
+    console.log();
+  })
 
   // emit login_request with token and website object
   // they check for fingerprint and send back
 
   socket.on('login_request_t1', function(req){
-    console.log(req);
+    if(req.website){
+      console.log(req);
+    }
     // go to database, check if token id exists
     //          check if website login/password exists
     //              if token id does not exist, prompt registration socket.emit back to background.js
@@ -27,10 +32,7 @@ io.on('connection', function(socket){
   socket.on('request_approved_t2', function(req){
     // if authorizing is false, emit a request_denied_t2 event to t2
     // emit 'login' to t1 socket with login and password data retrieved from mongo
-  }
-
-  userSocket = socket;
-  console.log('socket connected!!!!');
+  })
 });
 
 
