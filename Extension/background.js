e3d45e2030bd5be8db310d617cdcbd3895621445
brianfakhoury <brianfakhoury@yahoo.com>
2017-07-14 23:30:18 -0700

    bk

--- Extension/background.js
+++ Extension/background.js
@@ -5,29 +5,23 @@ var socket = io('http://localhost:3000');
 chrome.tabs.onUpdated.addListener(function(tabId, change, tab){
   // if not already logged in, prompt login
   //else do nothing, allow login
-  chrome.tabs.sendMessage(tabId, {}, function(obj){
+  chrome.tabs.sendMessage(tabId, {}, function(contentResponse){
     var website = null;
-    // TODO: if token does not exist, prompt registration and create new token
-    // TODO: create new token by prompting for a phone number.
-    // TODO: store the gmail/password/phone number object in mongo, store the ._id using chrome.storage.sync.set
-    /*
-    chrome.storage.sync.set({'token': theValue}, function() {
-      // Notify that we saved.
-      message('Settings saved');
-    });
-    */
     var token = 33454;
-
-    // Get current url *async*
+    // Get url for the tab
     chrome.tabs.get(tabId, function(tab){
       website = tab.url;
     })
-      // Send the request to the backend
-      socket.emit('login_request_t1', {token: token, website: website})
-      // Wait for backend approval to continue login process
-      socket.on('login_request_t2', function(obj) {
-        chrome.tabs.sendMessage(tabId, {username: obj.username, password: obj.password});
-      })
+    // Send the request to the backend to login to the site
+    socket.emit('login_request_t1', {token: token, website: website})
+    // Wait for backend approval to continue login process
+    // 
+    socket.on('login_request_t2', function(obj) {
+      chrome.tabs.sendMessage(tabId, {username: obj.username, password: obj.password});
+    })
+    socket.on('new_website', function(obj) {
+      chrome.tabs.sendMessage(tabId, {newSite: true});
+    })
 
       // socket.on('request_denied_t2') that reprompts login, mentioning the timeout or other error
   })
