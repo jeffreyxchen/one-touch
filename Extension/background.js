6ef0f5f64800daa6d64de76536c0abb64e242f0f
brianfakhoury <brianfakhoury@yahoo.com>
2017-07-15 01:53:45 -0700

    changes

--- Extension/background.js
+++ Extension/background.js
@@ -1,43 +1,38 @@
 var socket = io('http://localhost:3000');
-// Emit identity to server
+// Get the user token or register a new one
+var token;
+chrome.storage.sync.get('token', function(token) {
+  if(!token) {
+    socket.emit('register_t1')
+  } else {
+    token = token
+  }
+})
+socket.on('registration', function(obj){
+  chrome.storage.sync.set({token: obj.id})
+  console.log(id);
+})
+console.log('token',token);
 
 chrome.tabs.onUpdated.addListener(function(tabId, change, tab){
-  // if not already logged in, prompt login
-  //else do nothing, allow login
-  chrome.tabs.sendMessage(tabId, {}, function(contentResponse){
-    // Get the user token or register a new one
-    var token;
-    chrome.storage.sync.get('token', function(token) {
-      if(!token) {
-        socket.emit('register_t1')
-      } else {
-        token = token
-      }
-    })
-    console.log(token);
-
-    // Once new user info is stored in mongo, receive the event to register
-    // the user by storing the ._id of their mongo object as their token
-    // in local chrome storage
-    socket.on('registration', function(id){
-      chrome.storage.sync.set({token: id})
-      console.log(id);
-    })
     var website = null;
     // Get url for the tab
     chrome.tabs.get(tabId, function(tab){
       website = tab.url;
+      console.log(website);
+      // Check if we need to register this website to the user
+      socket.emit('check_new_website', {url: website})
+      socket.on('is_new_website', function(obj) {
+        if(obj.msg) {
+          chrome.tabs.sendMessage(tabId, {newSite: true});
+        } else {
+          // Send the request to the backend to login to the site
+          socket.emit('login_request_t1', {token: token, website: website})
+          // Wait for backend approval to unlock and continue login process
+          socket.on('login_approved_t2', function(credentials) {
+            chrome.tabs.sendMessage(tabId, {verified: true, username: credentials.username, password: credentials.password});
+          })
+        }
+      })
     })
-    // Send the request to the backend to login to the site
-    socket.emit('login_request_t1', {token: token, website: website})
-    // Wait for backend approval to continue login process
-    socket.on('login_request_t2', function(obj) {
-      chrome.tabs.sendMessage(tabId, {verified: true, username: obj.username, password: obj.password});
-    })
-    socket.on('new_website', function(obj) {
-      chrome.tabs.sendMessage(tabId, {newSite: true});
-    })
-
-      // socket.on('request_denied_t2') that reprompts login, mentioning the timeout or other error
   })
-})
