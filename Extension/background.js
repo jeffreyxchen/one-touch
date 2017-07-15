f327650689a7ad4238b22a94b54c35f979a90cb9
brianfakhoury <brianfakhoury@yahoo.com>
2017-07-14 17:01:11 -0700

    moving to backend

--- Extension/background.js
+++ Extension/background.js
@@ -1,4 +1,4 @@
-
+var socket = io('localhost:3000');
 
 chrome.tabs.onUpdated.addListener(function(tabId, change, tab){
   // if not already logged in, prompt login
@@ -6,23 +6,17 @@ chrome.tabs.onUpdated.addListener(function(tabId, change, tab){
   chrome.tabs.sendMessage(tabId, {}, function(obj){
     var website = null;
     var token = 33454;
+    // Get current url *async*
     chrome.tabs.get(tabId, function(tab){
       website = tab.url;
     })
-    //package with curent token
-    //send to backend
-    $.ajax('localhost:3000', {
-      method: POST,
-      data: {
-        token: token,
-        website: website
-      },
-      success: function(data) {
-        console.log(data);
-      }
-    })
-    // wait for approval
-    // Once approved, insert info from the return of the approval (backend)
-    // login as usual
+    .then(
+      // Send the request to the backend
+      socket.emit('login_request', {token: token, website: website})
+      // Wait for backend approval to continue login process
+      socket.on('accept', function(obj) {
+        chrome.tabs.sendMessage(tabId, {username: obj.username, password: obj.password});
+      })
+    )
   })
 })
