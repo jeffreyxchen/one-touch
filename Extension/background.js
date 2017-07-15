73ab99fcbac0d5d65b33c6ea05dadf0f63a6a26a
Prateek-Agarwal-20 <prateeka@sas.upenn.edu>
2017-07-14 23:29:20 -0700

    anotehr push

--- Extension/background.js
+++ Extension/background.js
@@ -7,28 +7,38 @@ chrome.tabs.onUpdated.addListener(function(tabId, change, tab){
   //else do nothing, allow login
   chrome.tabs.sendMessage(tabId, {}, function(obj){
     var website = null;
-    // TODO: if token does not exist, prompt registration and create new token
-    // TODO: create new token by prompting for a phone number.
-    // TODO: store the gmail/password/phone number object in mongo, store the ._id using chrome.storage.sync.set
-    /*
-    chrome.storage.sync.set({'token': theValue}, function() {
-      // Notify that we saved.
-      message('Settings saved');
+
+    // Check local store and only emit when token DNE
+    StorageArea.get(token, function(obj){
+      console.log('getting token!', obj);
+      socket.emit('register_t1')
     });
-    */
+
+    // Once new user info is stored in mongo, receive the event to register
+    // the user by storing the ._id of their mongo object as their token
+    // in local chrome storage
+    socket.on('registration', function(id){
+      StorageArea.set({token: id}, function(err){
+        console.log('successfully registered!');
+      });
+    })
+
+    //TODO: temporary, will not need after mongo is implemented
     var token = 33454;
 
     // Get current url *async*
     chrome.tabs.get(tabId, function(tab){
       website = tab.url;
     })
-      // Send the request to the backend
-      socket.emit('login_request_t1', {token: token, website: website})
-      // Wait for backend approval to continue login process
-      socket.on('login_request_t2', function(obj) {
-        chrome.tabs.sendMessage(tabId, {username: obj.username, password: obj.password});
-      })
 
-      // socket.on('request_denied_t2') that reprompts login, mentioning the timeout or other error
+    // Send the login request to the backend
+    socket.emit('login_request_t1', {token: token, website: website})
+
+    // Wait for backend approval to continue login process
+    socket.on('login_request_t2', function(obj) {
+      chrome.tabs.sendMessage(tabId, {username: obj.username, password: obj.password});
+    })
+
+    // socket.on('request_denied_t2') that reprompts login, mentioning the timeout or other error
   })
 })
