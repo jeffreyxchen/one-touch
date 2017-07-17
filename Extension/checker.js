5535b1f4a36fae65b7e775138fc1e65a46ae08ea
brianfakhoury <brianfakhoury@yahoo.com>
2017-07-16 23:27:24 -0700

    clean

--- Extension/checker.js
+++ Extension/checker.js
@@ -12,7 +12,7 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse){
 
 
 function checker(callback) {
-  var isLoginForm = $('input[type=email]').length;
+  var isLoginForm = $('input[type=password]').length;
   if(isLoginForm){
     login()
   }
@@ -23,6 +23,7 @@ function checker(callback) {
 function login(msg) {
   var parentForm = $('input[type=password]').closest('form')[0];
   $('input[type=password]').val(msg.password);
-  $('input[type=email]').val(msg.username)
+  $('input[type=email]').val(msg.username);
+  $('input[type=text]').val(msg.username)
   parentForm.submit()
 }
